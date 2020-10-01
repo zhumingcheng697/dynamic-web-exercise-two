e86ba32f6661adb7c1bf899871f5cd639fe003e7
McCoy Zhu <mz1945@nyu.edu>
2020-10-01 12:44:26 -0700

    Getting data from API

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,34 +1,79 @@
-import React from "react";
-
+import React, { useState, useEffect, useMemo } from "react";
+import axios from "axios";
 import Header from "../components/Header";
 
 const weatherKey = `533b80186fdf8e8d8faa837ee7ada898`;
 
 function Home() {
-  /*
-  Weather Type (ex. Cloudy)
-  Current Temperature
-  High Temperature
-  Low Temperature
-  Cloudiness
-  Humidity
-  Wind Speed
-  */
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState(`Tokyo`);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`
+      )
+      .then((response) => {
+        setWeatherData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [city]);
+
+  const {
+    weatherType,
+    currentTemp,
+    highTemp,
+    lowTemp,
+    cloudiness,
+    humidity,
+    windSpeed,
+  } = useMemo(() => {
+    let weatherType = "Loading...";
+    let currentTemp = "Loading...";
+    let highTemp = "Loading...";
+    let lowTemp = "Loading...";
+    let cloudiness = "Loading...";
+    let humidity = "Loading...";
+    let windSpeed = "Loading...";
+
+    if (weatherData) {
+      weatherType = `${weatherData.weather[0].description}`;
+      currentTemp = `${weatherData.main.temp}`;
+      highTemp = `${weatherData.main.temp_max}`;
+      lowTemp = `${weatherData.main.temp_min}`;
+      cloudiness = `${weatherData.clouds.all}%`;
+      humidity = `${weatherData.main.humidity}%`;
+      windSpeed = `${weatherData.wind.speed}km/h`;
+    }
+
+    return {
+      weatherType,
+      currentTemp,
+      highTemp,
+      lowTemp,
+      cloudiness,
+      humidity,
+      windSpeed,
+    };
+  }, [weatherData]);
+
   return (
     <>
       <Header />
-      <div className="Home">
-        <h2>Weather in Shanghai</h2>
-        <main>
-          <p>Weather Type: Cloudy</p>
-          <p>Current Temperature: 23 degrees</p>
-          <p>High Temperature: 28 degrees</p>
-          <p>Low Temperature: 18 degrees</p>
-          <p>Cloudiness: 100</p>
-          <p>Humidity: 35%</p>
-          <p>Wind Speed: 2km/h</p>
-        </main>
-      </div>
+      <main className="Home">
+        <h2>Weather in {city}</h2>
+        <div className="WeatherInfo">
+          <p>Weather Type: {weatherType}</p>
+          <p>Current Temperature: {currentTemp}</p>
+          <p>High Temperature: {highTemp}</p>
+          <p>Low Temperature: {lowTemp}</p>
+          <p>Cloudiness: {cloudiness}</p>
+          <p>Humidity: {humidity}</p>
+          <p>Wind Speed: {windSpeed}</p>
+        </div>
+      </main>
     </>
   );
 }
