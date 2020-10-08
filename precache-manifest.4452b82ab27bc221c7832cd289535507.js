fff5f46f193b86fdeaca27e800fc9f3c50d20dff
McCoy Zhu <mz1945@nyu.edu>
2020-10-08 14:36:39 -0700

    Updates

---
+++ precache-manifest.4452b82ab27bc221c7832cd289535507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56055fcdfccf6ed2836433974b88204",
+    "url": "/zhumingcheng697.github.io/dynamic-web-exercise-two/index.html"
+  },
+  {
+    "revision": "5233fd0d2f78cab61c8e",
+    "url": "/zhumingcheng697.github.io/dynamic-web-exercise-two/static/css/main.bbccf0c5.chunk.css"
+  },
+  {
+    "revision": "6539d725dc0e59e1b591",
+    "url": "/zhumingcheng697.github.io/dynamic-web-exercise-two/static/js/2.ad9a58ad.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/zhumingcheng697.github.io/dynamic-web-exercise-two/static/js/2.ad9a58ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5233fd0d2f78cab61c8e",
+    "url": "/zhumingcheng697.github.io/dynamic-web-exercise-two/static/js/main.70db7741.chunk.js"
+  },
+  {
+    "revision": "8026d4db69c7a389353c",
+    "url": "/zhumingcheng697.github.io/dynamic-web-exercise-two/static/js/runtime-main.7ac565e3.js"
+  }
+]);
