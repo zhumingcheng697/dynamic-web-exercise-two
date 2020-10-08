46b7fd12f06666ff869fa3201cc9c6159986ff1e
McCoy Zhu <mz1945@nyu.edu>
2020-10-08 16:22:17 -0700

    Changing anchor tag href

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ function Header() {
     <header className="Header">
       <h1>Weather App</h1>
       <nav>
-        <a href="/?city=Shanghai">Shanghai</a>
-        <a href="/?city=New York">New York</a>
-        <a href="/?city=Tokyo">Tokyo</a>
-        <a href="/?city=Moscow">Moscow</a>
+        <a href="./?city=Shanghai">Shanghai</a>
+        <a href="./?city=New York">New York</a>
+        <a href="./?city=Tokyo">Tokyo</a>
+        <a href="./?city=Moscow">Moscow</a>
       </nav>
     </header>
   );
